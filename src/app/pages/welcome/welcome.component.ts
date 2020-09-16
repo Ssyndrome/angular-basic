d91a15a07c3ab7ca46d1a5763e5d0348f9246596
ya xu <yaxu@thoughtworks.com>
2020-09-16 15:39:45 +0800

    add selections for table

--- src/app/pages/welcome/welcome.component.ts
+++ src/app/pages/welcome/welcome.component.ts
@@ -1,7 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 
 interface ItemData {
-  id: string;
+  id: number;
   name: string;
   age: string;
   address: string;
@@ -17,6 +17,32 @@ export class WelcomeComponent implements OnInit {
   i = 0;
   editId: string | null = null;
   listOfData: ItemData[] = [];
+  checked = false;
+  indeterminate = false;
+  setOfCheckedId = new Set<number>();
+
+  updateCheckedSet(id: number, checked: boolean): void {
+    if (checked) {
+      this.setOfCheckedId.add(id);
+    } else {
+      this.setOfCheckedId.delete(id);
+    }
+  }
+
+  onItemChecked(id: number, checked: boolean): void {
+    this.updateCheckedSet(id, checked);
+    this.refreshCheckedStatus();
+  }
+
+  refreshCheckedStatus(): void {
+    this.checked = this.listOfData.every(item => this.setOfCheckedId.has(item.id));
+    this.indeterminate = this.listOfData.some(item => this.setOfCheckedId.has(item.id)) && !this.checked;
+  }
+
+  onAllChecked(value: boolean): void {
+    this.listOfData.forEach(item => this.updateCheckedSet(item.id, value));
+    this.refreshCheckedStatus();
+  }
 
   startEdit(id: string): void {
     this.editId = id;
@@ -30,7 +56,7 @@ export class WelcomeComponent implements OnInit {
     this.listOfData = [
       ...this.listOfData,
       {
-        id: `${this.i}`,
+        id: this.i,
         name: `Edward King ${this.i}`,
         age: '32',
         address: `London, Park Lane no. ${this.i}`
@@ -39,7 +65,7 @@ export class WelcomeComponent implements OnInit {
     this.i++;
   }
 
-  deleteRow(id: string): void {
+  deleteRow(id: number): void {
     this.listOfData = this.listOfData.filter(d => d.id !== id);
   }
 
